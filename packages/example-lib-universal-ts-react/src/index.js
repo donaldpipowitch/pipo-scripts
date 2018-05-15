4833bd9afc09201c0655a6d1132dabe15416f0f8
Donald Pipowitch <pipo@senaeh.de>
2018-05-15 09:15:08 +0200

    fix setup for example

--- packages/example-lib-universal-ts-react/src/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var greeting_1 = require("./greeting");
-exports.Greeting = greeting_1.Greeting;
-var smile_1 = require("./smile");
-exports.Smile = smile_1.Smile;
-var cross_1 = require("./cross");
-exports.Cross = cross_1.Cross;
--- packages/example-lib-universal-ts-react/src/smile.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var react_1 = require("react");
-require("./smile.css");
-/**
- * Shows a smile by using a background image.
- */
-exports.Smile = function () { return react_1["default"].createElement("div", { className: "smile" }); };
