c4bf289f87d55150b49fdd49fd30a3285afd03d4
Donald Pipowitch <pipo@senaeh.de>
2018-06-04 12:39:24 +0200

    fix lint

--- packages/pipo-scripts/src/index.ts
+++ packages/pipo-scripts/src/index.ts
@@ -1,5 +1,5 @@
-export { getJestConfig } from "./jest.config";
-export { getWebpackConfig } from "./webpack.config";
-export { getBabelConfig } from "./babelrc";
-export { getEslintConfig } from "./eslintrc";
-export { getPrettierConfig } from "./prettier.config";
+export { getJestConfig } from './jest.config';
+export { getWebpackConfig } from './webpack.config';
+export { getBabelConfig } from './babelrc';
+export { getEslintConfig } from './eslintrc';
+export { getPrettierConfig } from './prettier.config';
--- prettier.config.js
+++ prettier.config.js
@@ -1,2 +1,1 @@
-module.exports = require("pipo-scripts").getPrettierConfig();
-// foo
+module.exports = require('pipo-scripts').getPrettierConfig();
